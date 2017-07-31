ef45af333d3d0c39e9a76a0e95d2e712989a04f3
Paulo Ferrato <pvpferrato@hotmail.com>
2017-07-31 19:27:28 -0300

    first commit

---
+++ Source/Teste1/Teste1.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "Teste1.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Teste1, "Teste1" );
